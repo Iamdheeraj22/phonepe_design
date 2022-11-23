0cb7666252341b0e50b8ef30ec316db683f631cc
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-11-23 12:06:22 +0530

    Complete Designing of  New UPI Id

--- lib/Page/AddNewUpiPage.dart
+++ lib/Page/AddNewUpiPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:phonepe_design/Utils/Colors.dart';
 import 'package:phonepe_design/Utils/Common.dart';
@@ -14,6 +16,7 @@ class _AddNewUpiPageState extends State<AddNewUpiPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: white,
         appBar: AppBar(
           leading: IconButton(
@@ -68,7 +71,7 @@ class _AddNewUpiPageState extends State<AddNewUpiPage> {
                       ),
                       //Type your Upi id
                       heightBox(25),
-                      TextField(
+                      const TextField(
                         style: TextStyle(color: black),
                         decoration: InputDecoration(
                             hintText: "Preferred ID",
@@ -77,19 +80,90 @@ class _AddNewUpiPageState extends State<AddNewUpiPage> {
                               style: TextStyle(
                                   color: black, fontWeight: FontWeight.bold),
                             ),
-                            suffixStyle: TextStyle(color: black),
+                            suffixStyle: TextStyle(
+                              color: black,
+                            ),
                             focusColor: primaryBlueColor),
                       )
                     ],
                   ),
                 ),
-                Spacer(),
+                heightBox(30),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Suggested ",
+                    style: TextStyle(
+                        color: black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+                Container(
+                  height: height(context) / 3,
+                  child: ListView.builder(
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          onTap: () {},
+                          title: Text(
+                            "dhivamprajapat${customNuber()}",
+                            style: TextStyle(
+                                color: black, fontWeight: FontWeight.normal),
+                          ),
+                          trailing: Container(
+                            height: 20,
+                            width: 20,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border.all(color: black)),
+                          ),
+                        );
+                      }),
+                ),
+                const Spacer(),
+
+                //Bottom button & footer header
                 Container(
                   child: Column(
-                    children: [Text("hii")],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "To create UPI ID, we have to send an SMS you mobile to validate and verify your device, standard SMS Changes ma apply.",
+                          style: TextStyle(
+                              color: grey,
+                              fontSize: 9,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          decoration: const BoxDecoration(color: grey),
+                          alignment: Alignment.center,
+                          child: const Text(
+                            "CREATE",
+                            style: TextStyle(
+                                color: white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 )
               ],
             )));
+  }
+
+  int customNuber() {
+    Random random = Random();
+    int result = random.nextInt(100) + 899;
+    return result;
   }
 }
